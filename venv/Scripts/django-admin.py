6bd1434d4f3a5ce7c029c5aebe5e96810e14c672
Shyam <67902666+shyam2511@users.noreply.github.com>
2022-06-12 05:27:06 +0530

    hello

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\nvshy\documents\github\valorant\valorant-elo\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
